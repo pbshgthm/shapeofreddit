1bfc0ac28f20d1dbc904729487ec7c9d2b3fc3cc
wanderling <poobeshgowtham@live.com>
2019-12-13 17:56:35 +0530

    firebase

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -4,13 +4,7 @@ import random
 from flask import Flask, render_template, jsonify
 
 
-reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
-                     client_id='zW7Pp8V-5pvw9A', client_secret="WtmRNzCOIm-armXEO_scVunHxDU")
-
-app = Flask(__name__)
-postId='e4ymg6'
-
-def build_tree(postId):
+def build_tree(reddit,postId):
 	print('getting - ',postId)
 	submission = reddit.submission(id=postId)
 	c_count=submission.num_comments
@@ -19,19 +13,18 @@ def build_tree(postId):
 	time=submission.created
 	points=submission.score
 	submission.comments.replace_more(limit=None)
-	comment_queue = submission.comments[:]  # Seed with top-level
+	comment_queue = submission.comments[:]
 
 	data=[["",postId]]
 	for comment in submission.comments.list():
-	    data.append([comment.parent_id.split('_')[-1],comment.id,comment.score,comment.created])
+	    data.append([comment.parent_id.split('_')[-1],comment.id])
 
 	return {'data':data,'link':permalink,'c_count':c_count,'title':title,'time':time,'points':points}
     
-def get_pid(subr):
+def get_pid(reddit,subr):
 	print('getting r/',subr)
 	submissions = reddit.subreddit(subr).hot()
 	
-	#'''
 	pref={
 		0:[],
 		1:[],
@@ -54,34 +47,28 @@ def get_pid(subr):
 
 	print('got',len(pid_list))
 	return pid_list
-	'''
-	pref=[[i.id,i.num_comments] for i in submissions]
-	pref.sort(key=lambda x:x[1])
-	pref=[i[0] for i in pref]
-	pref=pref[70:80]
-	'''
-	return pref
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+	
 
 
-@app.route('/get/<pid>')
-def get_tree_p(pid):
-	data=build_tree((pid))
-	return jsonify({'data':data})
+def get_tree(request):
 
-@app.route('/r/<subr>')
-def get_tree(subr):
+	request_json = request.get_json()
+	if request.args and 'subr' in request.args:
+		subr=request.args.get('subr')
+	elif request_json and 'subr' in request_json:
+		subr=request_json['subr']
+	else:
+		return "oops!"
+    
 
-	pid_list=get_pid(subr)
+	reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
+                     client_id='zW7Pp8V-5pvw9A', client_secret="WtmRNzCOIm-armXEO_scVunHxDU")
+	pid_list=get_pid(reddit,subr)
 	random.shuffle(pid_list)
-	return jsonify(build_tree(pid_list[0]))
+	return build_tree(reddit,pid_list[0])
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000',debug=True)
+
 
 
 
